c35bf94ef3b9f91876ae38650a7caf0ebb324de4
Vadim Voituk <vadim@voituk.com>
2014-12-28 12:28:12 +0100

    Code formatting

--- src/com/appkernel/view/AwesomeText.java
+++ src/com/appkernel/view/AwesomeText.java
@@ -9,81 +9,82 @@ import android.widget.TextView;
 import com.appkernel.commons.R;
 
 public class AwesomeText extends TextView {
-	
-	public static class Font {
-		
-		private static Typeface awesomeFont;
-			
-		public static synchronized Typeface get(Context context) {
-			if (awesomeFont == null)
-				awesomeFont = Typeface.createFromAsset(context.getAssets(), context.getString(R.string.AwesomeText_FontFile));
-			return awesomeFont;
-		}
-	}
-	
-	/**
-	 * 
-	 * @return Icon special character or empty string if no icon defined
-	 */
-	static CharSequence getIconAttributeValue(Context context, AttributeSet attrs) {
-		final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AwesomeText);
-		
-		StringBuilder icon = null;
-		
-		// iconId
-		int code = a.getInteger(R.styleable.AwesomeText_iconId, 0);
-		if (code > 0) {
-			icon = new StringBuilder();
-			for (char c : Character.toChars(code)) {
-	            icon.append(c);
+
+    public static class Font {
+
+        private static Typeface awesomeFont;
+
+        public static synchronized Typeface get(Context context) {
+            if (awesomeFont == null) {
+                awesomeFont = Typeface.createFromAsset(context.getAssets(), context.getString(R.string.AwesomeText_FontFile));
             }
-		}	
-		
-		a.recycle();
-		
-		return icon == null ? "" : icon;
-	}
-	
-	public static CharSequence _text(CharSequence icon, CharSequence text) {
-		StringBuilder s = new StringBuilder();
-		
-		if (icon != null && icon.length()>0) {
-			s.append(icon).append("  ");
-		}
-		
-		return s.append(text.toString().replaceAll("і", "i")); // replace Ukrainian i to latin i because FontAwesome do not support ukrainian one
-	}
+            return awesomeFont;
+        }
+    }
 
-	
-	private CharSequence icon  = "";
-	
-	public AwesomeText(Context context) {
-		super(context);
-		init(context);
-	}
-	
-	public AwesomeText(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init(context);
+    /**
+     * @return Icon special character or empty string if no icon defined
+     */
+    static CharSequence getIconAttributeValue(Context context, AttributeSet attrs) {
+        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AwesomeText);
 
-		icon = getIconAttributeValue(context, attrs);
-		
-		setText(_text(icon, super.getText())); 
-	}
-	
-	/**
-	 * Use this instead of Fucking final setText()!!!
-	 * @param text
-	 */
-	public void setTextWithIcon(CharSequence text) {
-		setText(_text(icon, text));
-	}
-	
-	private void init(Context context) {
-		if (isInEditMode())
-			return;
-		setTypeface(Font.get(context));
-	}
-		
+        StringBuilder icon = null;
+
+        // iconId
+        int code = a.getInteger(R.styleable.AwesomeText_iconId, 0);
+        if (code > 0) {
+            icon = new StringBuilder();
+            for (char c : Character.toChars(code)) {
+                icon.append(c);
+            }
+        }
+
+        a.recycle();
+
+        return icon == null ? "" : icon;
+    }
+
+    public static CharSequence _text(CharSequence icon, CharSequence text) {
+        StringBuilder s = new StringBuilder();
+
+        if (icon != null && icon.length() > 0) {
+            s.append(icon).append("  ");
+        }
+
+        return s.append(text.toString().replaceAll("і", "i")); // replace Ukrainian i to latin i because FontAwesome do not support ukrainian one
+    }
+
+
+    private CharSequence icon = "";
+
+    public AwesomeText(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public AwesomeText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+
+        icon = getIconAttributeValue(context, attrs);
+
+        setText(_text(icon, super.getText()));
+    }
+
+    /**
+     * Use this instead of Fucking final setText()!!!
+     *
+     * @param text
+     */
+    public void setTextWithIcon(CharSequence text) {
+        setText(_text(icon, text));
+    }
+
+    private void init(Context context) {
+        if (isInEditMode())
+            return;
+        setTypeface(Font.get(context));
+    }
+
 }
 
