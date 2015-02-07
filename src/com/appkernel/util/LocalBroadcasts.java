a6b91c46525810948e8098ac8431eb56aae306cc
Vadim Voituk <vadim@voituk.com>
2015-02-07 23:07:05 +0100

    LocalBroadcasts now supports @Nullable contextes

--- src/com/appkernel/util/LocalBroadcasts.java
+++ src/com/appkernel/util/LocalBroadcasts.java
@@ -4,17 +4,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 
 /** 
  * Helper class for local broadcasts management
  *  
- * @author vadim
+ * @author Vadym Voitiuk <vadim@voituk.com>
  *
  * @todo: Implement InterFilter Cache
  */
 public class LocalBroadcasts {
-    
     
     public static void send(final Context context, final Intent intent) {
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
@@ -24,13 +24,26 @@ public class LocalBroadcasts {
         LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(action));
     }
     
-    public static void register(final Context context, BroadcastReceiver receiver, IntentFilter filter) {
+    public static void register(@Nullable final Context context, BroadcastReceiver receiver, IntentFilter filter) {
+        if (context == null)
+            return;
         LocalBroadcastManager.getInstance(context).registerReceiver(receiver, filter);
     }
-    
-    
-    public static void unregister(final Context context, final BroadcastReceiver receiver) {
-        LocalBroadcastManager.getInstance(context).unregisterReceiver(receiver);
+
+    public static void register(@Nullable final Context context, BroadcastReceiver receiver, String action) {
+        if (context == null)
+            return;
+        LocalBroadcastManager.getInstance(context).registerReceiver(receiver, new IntentFilter(action)); //TODO: use IntentFilter cache here
+    }
+
+    public static void unregister(@Nullable final Context context, final BroadcastReceiver... receivers) {
+        if (context == null)
+            return;
+        final LocalBroadcastManager manager = LocalBroadcastManager.getInstance(context);
+        for(BroadcastReceiver receiver: receivers) {
+            if (receiver != null)
+                manager.unregisterReceiver(receiver);
+        }
     }
 
 }
